3da9a648252a95703f57221515767621378b40c5
JoseCabrejoVillarCampus <cabre920903@hotmil.com>
2023-02-14 20:26:12 -0500

    fix[ejercicio2]

--- ejercicioFun2.py
+++ ejercicioFun2.py
@@ -10,26 +10,15 @@ será de 500 millones. El récord esta en 15,50 metros"""
 
 
 def AgregarAtletas():
-    atletasMarcas=[]
-    cantidadAtletas = int(input("Cuantos atletas desea agregar: "))
+    atletasMarcas={}
+    cantidadAtletas = int(input("\nCuantos atletas desea agregar: "))
     for x in range(cantidadAtletas):
         nombre = input("Ingrese el nombre del atleta: ")
         marca= float(input("Ingrese la marca del atleta: "))
         atletaDic={"nombre":nombre,"marca":marca}
-        atletasMarcas.append(atletaDic)
+        atletasMarcas[nombre,marca]=atletaDic
         if (marca>15.50):
             print ("El Atleta rompio el record de 15.50 metros \n el pago que será de 500 millones")
-        
+        ordenados = sorted(atletasMarcas, key=lambda  metros : metros[0])
+        print(ordenados)
 AgregarAtletas()
-
-"""
-['jua', 'Car']
-[ 23 ,    35]
-
-max
-posi
-A[i]
-for i in range(A):
-    A[i] > max
-    max =
-    posi = i"""
